6412518aeff6fb8a0894039d0584fe3d973cfad1
itsDalcaN <dsumindan@oakland.edu>
2021-11-07 19:53:51 -0500

    Validation methods for player moves

--- javascript/util.js
+++ javascript/util.js
@@ -1,7 +1,55 @@
 function getMove(player, board) {
   // TODO: Determine valid moves
+  let validMoves = []
+
+  for ( let row = 2; row < board.length; row++ ) {
+    for ( let column = 2; column < board[0].length; column++ ) {
+      if ( !isOpenSquare( board, row, column ) ) { continue }
+
+      let enemyPlayer = (player == 1)? 2 : 1 
+      if ( !isEnemyPieceAround( enemyPlayer, board, row, column ) ) { continue }
+      if ( !isOutflankPlacement( player, enemyPlayer, board, row, column ) ) { continue }
+
+      validMoves.push( [row, column] )
+    } 
+  } 
+
+  console.log(validMoves)
+
   // TODO: Determine best move
   return [2, 3];
+}
+
+function isOpenSquare( board, row, column ) { return board[row][column] == 0 }
+function isEnemyPieceAround( enemyPlayer, board, row, column ) {
+  for ( let r = row - 1; r <= row + 1 && r < 8; r++ ) {
+    for ( let c = column - 1; c <= column + 1 && c < 8; c++ ) {
+      if ( board[r][c] == enemyPlayer ) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
+function isOutflankPlacement( player, enemyPlayer, board, row, column ) {
+  return isHorizontalOutflankPlacement( player, enemyPlayer, board, row, column ) 
+    || isVerticalOutflankPlacement( player, enemyPlayer, board, row, column )
+    || isDiagonalOutflankPlacement( player, enemyPlayer, board, row, column )
+}
+
+function isHorizontalOutflankPlacement( player, enemyPlayer, board, row, column ) {
+  // If player places their stone at row and column, does it outflank/sandwich the enemy?
+  return false
+}
+
+function isVerticalOutflankPlacement( player, enemyPlayer, board, row, column ) {
+  return false
+}
+
+function isDiagonalOutflankPlacement( player, enemyPlayer, board, row, column ) {
+  return false
 }
 
 function prepareResponse(move) {
